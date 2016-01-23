680ee189b253f860d944eb99fb2abdcf70868d23
Jim Sutton <jimsuttonjimsutton@gmail.com>
2016-01-23 13:26:59 -0700

    reading todos from mirage server

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,19 +1,9 @@
 export default function() {
 
-  // These comments are here to help you get started. Feel free to delete them.
-
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
-  /*
-    Route shorthand cheatsheet
-  */
   /*
     GET shorthands
 
@@ -52,31 +42,14 @@ export default function() {
     this.del('/contacts/:id', ['contact', 'addresses']);
   */
 
-  /*
-    Function fallback. Manipulate data in the db via
-
-      - db.{collection}
-      - db.{collection}.find(id)
-      - db.{collection}.where(query)
-      - db.{collection}.update(target, attrs)
-      - db.{collection}.remove(target)
-
-    // Example: return a single object with related models
-    this.get('/contacts/:id', function(db, request) {
-      var contactId = +request.params.id;
-
-      return {
-        contact: db.contacts.find(contactId),
-        addresses: db.addresses.where({contact_id: contactId})
-      };
-    });
-
-  */
-}
-
-/*
-You can optionally export a config that is only loaded during tests
-export function testConfig() {
+  this.get('/todos', function() {
+    return { 
+      todos: [
+        {id: 1, title: 'First todo'},
+        {id: 2, title: 'Second todo'},
+        {id: 3, title: 'Third todo'},
+      ]
+    };
+  });
 
 }
-*/
