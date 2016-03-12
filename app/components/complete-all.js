b3d6b13fc3127dcc116619a06314afa0e128e5ea
Jim Sutton <jimsuttonjimsutton@gmail.com>
2016-03-12 11:24:50 -0700

    create "todos-list" as parent for todo-item and use for global/model-level concerns

--- app/components/complete-all.js
+++ app/components/complete-all.js
@@ -6,30 +6,7 @@ export default Component.extend({
   tagName: 'p',
   classNames: ['complete-all'],
 
-  hasTodos: computed('model.length', function() {
-    return this.get('model.length') > 0
-  }),
-
-  allCompleted: computed('model.@each.complete', function() {
-    return this.get('hasTodos') && this.get('model').isEvery('complete');
-  }), //better way to do this in newer versions of Ember?
-
-  toggleAll: function(value) {
-    //is there a better way to update the view? setting it in route doesn't check the boxes either
-    $('.mark-as-complete').prop('checked', value);
-
-    let todos = this.get('model');
-    todos.forEach((todo) => {
-      todo.set('complete', value);
-      this.sendAction('update', todo);
-    });
-  },
-
   click() {
-    if (!this.get('allCompleted')) {
-      this.toggleAll(true);
-    } else if (this.get('allCompleted')) {
-      this.toggleAll(false);
-    }
+    this.sendAction('action');
   }
 });
---
+++ app/components/todos-list.js
@@ -0,0 +1,48 @@
+import Ember from 'ember';
+
+const { computed, $, Component } = Ember;
+
+export default Component.extend({
+  hideCompleted : false,
+
+  hasTodos: computed('model.length', function() {
+    return this.get('model.length') > 0;
+  }),
+
+  allCompleted: computed('model.@each.complete', function() {
+    return this.get('hasTodos') && this.get('model').isEvery('complete');
+  }), //better way to do this in newer versions of Ember?
+
+  toggleCompleted: function(value) {
+    //is there a better way to update the view? setting it in route doesn't check the boxes either
+    $('.mark-as-complete').prop('checked', value);
+
+    let todos = this.get('model');
+    todos.forEach((todo) => {
+      todo.set('complete', value);
+      this.sendAction('update', todo);
+    });
+  },
+
+  actions: {
+    update(data) {
+      this.sendAction('update', data);
+    },
+    deleteTodo(data) {
+      this.sendAction('deleteTodo', data);
+    },
+
+    toggleComplete() {
+      if (!this.get('allCompleted')) {
+        this.toggleCompleted(true);
+      } else if (this.get('allCompleted')) {
+        this.toggleCompleted(false);
+      }
+    },
+    toggleVisible() {
+      console.log("hitting toggle visible");
+      console.log("hideCompleted = ", this.get('hideCompleted'));
+      this.toggleProperty('hideCompleted');
+    }
+  }
+});
